79b247ef3076121669b0265c1f322dfc425d5e9c
JAY PANARA <160013245+jaypanara019@users.noreply.github.com>
2025-05-12 11:50:31 +0530

    5-12-25(c++)

---
+++ task-2/Q-1.cpp
@@ -0,0 +1,30 @@
+// 1.Addition of 12 and 6 is 18
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int num = 12, num2 = 6;
+
+    cout << " addition of " << num << " " << " and " << num2 << " " << " is "  << num + num2 << endl;
+    cout << " addition of " << num << " " << " and " << num2 << " " << " is " << num - num2 << endl;
+    cout << " addition of " << num << " " << " and " << num2 << " " << " is " << num * num2 << endl;
+    cout << " addition of " << num << " " << " and " << num2 << " " << " is " << num % num2 << endl;
+    cout << " addition of " << num << " " << " and " << num2 << " " << " is " << num / num2 << endl;
+
+    cout << num << " + " << num2 << " = " << num + num2 << endl;
+    cout << num << " + " << num2 << " = " << num - num2 << endl;
+    cout << num << " + " << num2 << " = " << num % num2 << endl;
+    cout << num << " + " << num2 << " = " << num / num2 << endl;
+    cout << num << " + " << num2 << " = " << num * num2 << endl;    
+};
+
+
+
+ 
+
+
+
+
+
+// cout << num << " + " << num2 << " = " << num + num2 << endl;
